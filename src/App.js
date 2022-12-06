480b1a6a70bd3236b0a0a1189c64a4d3501dfe51
Hussein <yusufhuszy@gmail.com>
2022-12-06 16:15:32 +0100

    many changes made

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ export default function App() {
   return (
     <div>
               <Routes>
+          
           <Route path='/' element={<Home />} />
 
           <Route path='Signup' element={<Signup />}/>
